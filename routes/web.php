657c45719062b8feb00a533984f85cf6d32d479f
JoseGroo <jose_manuel2818@hotmail.com>
2024-09-02 21:42:39 -0700

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\AngularController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::any('/{any}', [AngularController::class, 'index'])->where('any', '^(?!api).*$');
